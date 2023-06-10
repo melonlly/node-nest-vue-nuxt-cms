001aebed4f01ea9802ee7be4e961c60ea8aeec02
bob <1440155977@qq.com>
2023-06-10 20:42:54 +0800

    完了

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,8 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './index.less';
+import {Descriptions, Tag} from '@douyinfe/semi-ui';
+import {IconArticle} from "@douyinfe/semi-icons";
+import {Table} from '@douyinfe/semi-ui';
 
 const Home = () => {
+  const columns = [
+    {
+      title: '序号',
+      dataIndex: 'index',
+    },
+    {
+      title: '考试科目',
+      dataIndex: 'subject',
+    },
+    {
+      title: '考试成绩',
+      dataIndex: 'grade',
+    },
+  ];
   return (
-    <div>home</div>
+    <div className={styles.container}>
+      <div className={styles.head}>
+        <span>云南开放大学 2023年 春季招生</span>：<strong>军地联合培养方案（2）</strong>
+        <span className={styles.stud}>学籍状态：<span className={styles.status}>在籍</span></span>
+      </div>
+      <li className={styles.items}>
+        <div className={styles.title}>
+          <IconArticle style={{color: '#FFD54F', fontSize: 19, marginLeft: 10}}/><h3>学生基本信息</h3>
+        </div>
+        <div className={styles.content}>
+          <Descriptions align="left">
+            <Descriptions.Item itemKey="姓名">1,480,000</Descriptions.Item>
+            <Descriptions.Item itemKey="民族">98%</Descriptions.Item>
+            <Descriptions.Item itemKey="证件类型">3级</Descriptions.Item>
+            <Descriptions.Item itemKey="学习中心">电商</Descriptions.Item>
+            <Descriptions.Item itemKey="中心电话">未认证</Descriptions.Item>
+            <Descriptions.Item itemKey="通讯地址">未认证</Descriptions.Item>
+            <Descriptions.Item itemKey="邮政编码">未认证</Descriptions.Item>
+          </Descriptions>
+          <Descriptions align="left">
+            <Descriptions.Item itemKey="性别">1,480,000</Descriptions.Item>
+            <Descriptions.Item itemKey="政治面貌">98%</Descriptions.Item>
+            <Descriptions.Item itemKey="证件号">3级</Descriptions.Item>
+            <Descriptions.Item itemKey="出生日期">电商</Descriptions.Item>
+            <Descriptions.Item itemKey="移动电话">未认证</Descriptions.Item>
+            <Descriptions.Item itemKey="Email">未认证</Descriptions.Item>
+          </Descriptions>
+          <img className={styles.userIMG} src='/' alt="无"/>
+        </div>
+      </li>
+      <li className={styles.items}>
+        <div className={styles.title}>
+          <IconArticle style={{color: '#FFD54F', fontSize: 19, marginLeft: 10}}/><h3>考试成绩汇总</h3>
+        </div>
+        <div className={styles.content}>
+          <Table
+            dataSource={[{index: 1, subject: '高数', grade: 105, type: 1}, {
+              index: 2,
+              subject: '语文',
+              grade: 105,
+              type: 2
+            }]}
+            groupBy={'type'}
+            bordered
+            rowKey={'index'}
+            columns={columns}
+            pagination={false}
+            renderGroupSection={groupKey => <strong>第 {groupKey} 学期</strong>}
+            onGroupedRow={(group, index) => {
+              return {
+                onClick: e => {
+                  console.log(`Grouped row clicked: `, group, index);
+                },
+              };
+            }}
+            clickGroupedRowToExpand // if you want to click the entire row to expand
+          />
+        </div>
+      </li>
+    </div>
   )
 }
 export default Home
--- web/src/pages/Login/index.jsx
+++ web/src/pages/Login/index.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { Card, Form, Button } from '@douyinfe/semi-ui';
+import React, {useState} from 'react';
+import {Card, Form, Button} from '@douyinfe/semi-ui';
 import styles from './index.less';
-import { IconKey, IconUser } from '@douyinfe/semi-icons';
+import {IconKey, IconUser} from '@douyinfe/semi-icons';
+import {history} from 'umi';
+import imgUrl from '../../../public/login-white_02.jpg';
 
 export default function Login() {
-  const [show,setShow] = useState(true);
+  const [show, setShow] = useState(true);
 
-  const handleSubmit =(values) => {
-
+  const handleSubmit = (values) => {
+    history.replace('/home');
   }
 
   const validate1 = (val, values) => {
-    if(!val){
+    if (!val) {
       return '请输入用户名';
     }else if(/(\s+)/g.test(val)){
       return '不能有空格';
@@ -32,16 +34,17 @@ export default function Login() {
     <div className={ styles.login }>
       <Card shadows='always'
             style={{ width: 450,height:300,display:'flex', alignItems:'center',justifyContent:'center'}}>
+        <img src={imgUrl} alt="1"/>
         <Form layout='vertical'
               labelPosition='left'
               labelAlign='right'
               labelWidth={60}
-              style={{ width: 400 }}
+              style={{width: 400}}
               onSubmit={handleSubmit}>
           <Form.Input field='username'
-                      prefix={<IconUser />}
+                      prefix={<IconUser/>}
                       validate={validate1}
-                      label='用户名'  placeholder='请输入用户名'/>
+                      label='用户名' placeholder='请输入用户名'/>
           {
             show ? (
               <Form.Input field='password'
