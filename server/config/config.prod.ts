fcacc6f02370118057a910cec603c935617bee96
melon <melonlly@outlook.com>
2023-06-16 10:14:37 +0800

    Update config.prod.ts

--- server/config/config.prod.ts
+++ server/config/config.prod.ts
@@ -4,7 +4,7 @@ export default () => ({
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    password: 'Kushuye777:',
+    password: '',
     database: 'sms-db',
     synchronize: false,
   },
