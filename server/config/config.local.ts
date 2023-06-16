7c847e3b040aa637ef69cae9490b45b103c57061
melon <melonlly@outlook.com>
2023-06-16 10:14:14 +0800

    Update config.local.ts

--- server/config/config.local.ts
+++ server/config/config.local.ts
@@ -4,7 +4,7 @@ export default () => ({
     host: '123.207.215.175',
     port: 3306,
     username: 'root',
-    password: 'Kushuye777:',
+    password: '',
     database: 'sms-db',
     synchronize: false,
   },
